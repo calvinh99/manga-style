4c8d706d4b23cdf83bd22f20cbe6e7d4bb4d648a
Calvin Huang <jibunhidoi999@gmail.com>
2022-09-17 11:45:30 -0400

    Changed separator for image tag

--- shell_scripts/update_tweets.sh
+++ shell_scripts/update_tweets.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run --rm --env-file .env.prod --network mangastyle_backend mangastyle_web:latest python manage.py update_tweets all
+# the separator used by aws ec2 docker-compose is a dash '-' whereas my macbook uses an underscore '_'
+# using dash since I need this in production for cron jobs
+docker run --rm --env-file .env.prod --network mangastyle_backend mangastyle-web:latest python manage.py update_tweets all
